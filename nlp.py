b267fb644e6c63d860f4c9aa55ab7bdc0c789fc8
achafi <assia.chafii@hotmail.com>
2020-01-09 13:31:42 +0100

    Stemming

--- nlp.py
+++ nlp.py
@@ -17,12 +17,15 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter="\t", quoting = 3)
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer 
 nltk.download('stopwords') # not relevant words
 
 review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0]) # Replace all eliminated chars with ' '
 print(review) # return Wow    Loved this place
 review = review.lower() # to lower case
 review = review.split() # list of words
-review = [word for word in review if not word in set(stopwords.words('english'))] #set to speed up the algo
+ps = PorterStemmer() # Stemming (Racinisation l'origine du mot loved -> love)
+review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] #set to speed up the algo
+review = ' '.join(review)
+print(review)
 
-print(review) # ['wow', 'loved', 'place']
