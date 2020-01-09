a321066ec449cb0416ccd424370b9e407ad29e9a
achafi <assia.chafii@hotmail.com>
2020-01-09 13:53:28 +0100

    for loop

--- nlp.py
+++ nlp.py
@@ -19,13 +19,17 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer 
 nltk.download('stopwords') # not relevant words
+corpus = []
+for i in range(dataset.shape[0]):
+    
+    review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i]) # Replace all eliminated chars with ' '
+    #print(review) # return Wow    Loved this place
+    review = review.lower() # to lower case
+    review = review.split() # list of words
+    ps = PorterStemmer() # Stemming (Racinisation l'origine du mot loved -> love)
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] #set to speed up the algo
+    review = ' '.join(review)
+    #print(review)
+    corpus.append(review)
 
-review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0]) # Replace all eliminated chars with ' '
-print(review) # return Wow    Loved this place
-review = review.lower() # to lower case
-review = review.split() # list of words
-ps = PorterStemmer() # Stemming (Racinisation l'origine du mot loved -> love)
-review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] #set to speed up the algo
-review = ' '.join(review)
-print(review)
-
+print(corpus)
