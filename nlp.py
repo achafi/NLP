4fccc3e0806780b3490cda533e8e7322ab573948
achafi <assia.chafii@hotmail.com>
2020-01-09 15:40:47 +0100

    train and predict

--- nlp.py
+++ nlp.py
@@ -41,7 +41,17 @@ X = cv.fit_transform(corpus).toarray()
 print(X)
 
 y = dataset.iloc[:,1].values
-X_train, y_train, X_test, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 # Fitting Random Forest Classifier to the training set
-from sklearn impo
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=10, criterion ='entropy', random_state =0)
+classifier.fit(X_train, y_train)
+
+# Predecting the Test set Results
+y_pred = classifier.predict(X_test)
+
+# Confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
